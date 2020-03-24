ed4f51a21fa592cfbbfaf93c9fcd8fc6f067e8a2
Simon <simonxd@gmail.com>
2020-03-23 21:52:41 -0300

    Updates

---
+++ precache-manifest.891fccdaf9779db4a6294e66bfa8000f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d736f002206ef531b1e75f5e8c2e4402",
+    "url": "/turquia-2019/index.html"
+  },
+  {
+    "revision": "69072b7e184a3144d413",
+    "url": "/turquia-2019/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6ada97c0308fb9b4170b",
+    "url": "/turquia-2019/static/css/main.8910beef.chunk.css"
+  },
+  {
+    "revision": "69072b7e184a3144d413",
+    "url": "/turquia-2019/static/js/2.8d6e8125.chunk.js"
+  },
+  {
+    "revision": "284d116f86c8eb92a654f6318b2938d9",
+    "url": "/turquia-2019/static/js/2.8d6e8125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ada97c0308fb9b4170b",
+    "url": "/turquia-2019/static/js/main.8b090e7a.chunk.js"
+  },
+  {
+    "revision": "f73c63af79733b715681",
+    "url": "/turquia-2019/static/js/runtime-main.07152e54.js"
+  }
+]);
